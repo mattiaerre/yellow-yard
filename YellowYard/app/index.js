65e6292dc15acda21d30a18ca61359bde6ddba83
mattia richetto <mattia.richetto@gmail.com>
2017-05-21 19:34:19 -0700

    add flip!

--- YellowYard/app/index.js
+++ YellowYard/app/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,14 +14,41 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
     fontFamily: 'Menlo-Bold'
+  },
+  flip: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    fontFamily: 'Menlo-Bold'
   }
 });
 
-const App = ({ name }) => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>{name}</Text>
-  </View>
-);
+// see: https://medium.freecodecamp.com/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb
+const reverseString = str => ((str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0));
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: props.name };
+    this.handlePress = this.handlePress.bind(this);
+  }
+
+  handlePress() {
+    const name = reverseString(this.state.name);
+    this.setState({ name });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>{this.state.name}</Text>
+        <TouchableOpacity onPress={this.handlePress}>
+          <Text style={styles.flip}>Flip!</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 App.propTypes = {
   name: PropTypes.string.isRequired
